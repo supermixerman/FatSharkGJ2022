a733f3bbe77215beec1358cf97124c648c61f5d4
Leorai <leo.raitaniemi@gmail.com>
2022-10-22 14:36:49 +0200

    Game Manager changes

--- Fat Shark GJ 2022/Assets/Scripts/GameManager.cs
+++ Fat Shark GJ 2022/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject playerPrefab, mainCamera;
     [SerializeField] int playerAmount, turn;
     [SerializeField] List<GameObject> playerList;
-    [SerializeField] Transform spawnLocation;
+    [SerializeField] List<Transform> spawnLocations;
     [SerializeField] List<Color> playerColorsList;
     
     public static GameManager gameManager;
@@ -29,22 +29,37 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void SpawnPlayers(){
-        for (int i = 0; i < playerList.Count; i++)
+    public void StartGame(int players){
+        playerAmount = players;
+        SpawnPlayers(playerAmount);
+        TurnStart();
+        //menuCanvas.SetActive(false);
+        //gameCanvas.SetActive(true);
+
+    }
+
+    public void SpawnPlayers(int amount){
+        if (amount > spawnLocations.Count) //There can not be more players than the amount of spawn locations.
         {
-            playerList.Add(Instantiate(playerPrefab, spawnLocation.position, Quaternion.identity));
+            Debug.LogError("Player Amount is too large. Add more Spawn Locations or lower the value. Changed player amount to the amount of spawn locations.");
+            amount = spawnLocations.Count;
+            playerAmount = spawnLocations.Count;
+        }
+        for (int i = 0; i < amount; i++)
+        {
+            playerList.Add(Instantiate(playerPrefab, spawnLocations[i].position, Quaternion.identity));
             playerList[i].gameObject.name = "Player " + (i+1);
             Debug.Log(playerList[i].gameObject.name);
-            if (playerColorsList.Count >= playerList.Count){ //Set player colour
-                //playerList[i].GetComponent<Player>().SetPlayerColor(playerColorsList[i]);
+            if (playerColorsList.Count >= playerList.Count){ //Set player color
+                playerList[i].GetComponent<SpriteRenderer>().color = playerColorsList[i];
             }
-            playerAmount += 1;
         }
     }
 
     public void CameraFollow(Transform target){
         Debug.Log("Camera Target: "+target);
-        mainCamera.transform.position = target.position;
+        mainCamera.transform.SetParent(target, false);
+        //mainCamera.transform.position = target.position;
     }
 
     public void TurnStart(){
@@ -52,5 +67,16 @@ public class GameManager : MonoBehaviour
         //characterController.SetActivePlayer(playerList[0]);
         CameraFollow(playerList[0].transform);
         //turnText.text = "Turn: "+playerList[0].name;
+    }
+
+    public void NextTurn(){
+        turn++;
+        if (turn >= playerList.Count){
+            Debug.Log("Reset turn");
+            turn = 0;
+        }
+        Debug.Log("Turn num = "+turn);
+        //characterController.SetActivePlayer(playerList[turn]);
+        CameraFollow(playerList[turn].transform);
     }
 }
